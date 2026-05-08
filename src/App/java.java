93561c4530799fc899a977fc8eceaf72dce72129
RiftyKopling <arsy1429@gmail.com>
2026-05-08 21:09:07 +0700

    first commit

---
+++ src/App/java.java
@@ -0,0 +1,17 @@
+package App;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author HP
+ */
+public class java {
+     public static void main(String[] args) {
+        MatchCards matchCard =  new MatchCards();
+    }
+}
